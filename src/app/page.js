3dc8d010676c576d7d6da1cce725bb1e032c5115
JawadAhmed07 <qwertykey41@gmail.com>
2024-09-29 17:02:41 +0500

    learning structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
+
+  const [name ,setName]=useState("(Your name here)")
+
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <p>This is a starter template for Next.js with TypeScript.</p>
-    </div>
+      <div>
+        <h1 className="p-4 text-center">
+          My name is {name}
+        </h1>
+        <button
+        className="border border-black p-2 flex mx-auto"
+        onClick={()=>{
+          setName(prompt("Enter your name"))
+        }}>Click to add your name</button>
+      </div>
+  
   );
 }
+
