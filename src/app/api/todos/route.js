ca28e372aa7574de77c47afcc69d41a5836812b9
JawadAhmed07 <qwertykey41@gmail.com>
2024-10-17 00:48:20 -0700

    api learning

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -3,20 +3,24 @@
 
 const Todos=[
     {
-        todo:"Tasssk 1",
+        todo:"Task 1",
         isCompleted:false,
+        id:1
     },
     {
         todo:"Task 2",
         isCompleted:false,
+        id:2
     },
     {
         todo:"Task 3",
         isCompleted:false,
+        id:3
     },
     {
         todo:"Task 4",
         isCompleted:false,
+        id:4
     },
 ]
 export async function GET(request){
@@ -25,3 +29,6 @@ export async function GET(request){
         msg:"Todos fetch successfully"
     })
 }
+
+// export async function GET(request){
+// }
--- src/app/todos/page.js
+++ src/app/todos/page.js
@@ -1,20 +1,29 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 // just pushing on github
 
 export default async function Todos() {
 
-    let res = await fetch("http://localhost:3000/api/todos")
+    let res = await fetch("http://localhost:3000/api/todos",{
+        cache:"no-cache"
+    })
     res = await res.json();
     return (
         <div className="min-h-screen p-10">
             <h1 className="text-3xl font-bold text-center">This is Todos page</h1>
+            <div className="w-2/3 mx-auto flex gap-2 ">
+                <input
+                placeholder="Add todo"
+                className="border border-purple-200 flex-grow p-2"
+                type="text"/>
+                <Button>Add todo</Button>
+            </div>
             {
                 res.data?.map((todo) => {
                     return (
-                        <Link key={todo} href={`/todo/${todo}`}>
-                        <h1 className="text-center my-5 itms-center border text-3xl w-2/3 mx-auto">{todo}</h1>
-                        <h1>todos fetched</h1>
+                        <Link key={todo.id} href={`/todo/${todo.id}`}>
+                        <h1 className="text-center my-5 itms-center border text-3xl w-2/3 mx-auto">{todo.todo}</h1>
                     </Link>
                     )
                 })
