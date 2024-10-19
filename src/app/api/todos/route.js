a1efa5364032141e2890e4e7d911b4d375b19297
JawadAhmed07 <qwertykey41@gmail.com>
2024-10-19 12:07:31 +0500

    learning next js

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -1,34 +1,49 @@
 
 
 
+// const Todos=[
+//     {
+//         todo:"Task 1",
+//         isCompleted:false,
+//         id:1
+//     },
+//     {
+//         todo:"Task 2",
+//         isCompleted:false,
+//         id:2
+//     },
+//     {
+//         todo:"Task 3",
+//         isCompleted:false,
+//         id:3
+//     },
+//     {
+//         todo:"Task 4",
+//         isCompleted:false,
+//         id:4
+//     },
+// ]
+// export async function GET(request){
+//     return Response.json({
+//         data:Todos,
+//         msg:"Todos fetch successfully"
+//     })
+// }
+
+// export async function GET(request){
+// }
 const Todos=[
-    {
-        todo:"Task 1",
-        isCompleted:false,
-        id:1
-    },
-    {
-        todo:"Task 2",
-        isCompleted:false,
-        id:2
-    },
-    {
-        todo:"Task 3",
-        isCompleted:false,
-        id:3
-    },
-    {
-        todo:"Task 4",
-        isCompleted:false,
-        id:4
-    },
+    "Todo1",
+    "Todo2",
+    "Todo3",
+    "Todo4",
+    "Todo5",
+    "Todo6",
 ]
-export async function GET(request){
+
+export async function GET(request) {
     return Response.json({
         data:Todos,
         msg:"Todos fetch successfully"
     })
 }
-
-// export async function GET(request){
-// }
--- src/app/todos/page.js
+++ src/app/todos/page.js
@@ -1,36 +1,36 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
 
-// just pushing on github
-// just pushing on github
+// // just pushing on github
+// // just pushing on github
 
 
 
-export default async function Todos() {
+// export default async function Todos() {
 
-    let res = await fetch("http://localhost:3000/api/todos",{
-        cache:"no-cache"
-    })
-    res = await res.json();
-    return (
-        <div className="min-h-screen p-10">
-            <h1 className="text-3xl font-bold text-center">This is Todos page</h1>
-            <div className="w-2/3 mx-auto flex gap-2 ">
-                <input
-                placeholder="Add todo"
-                className="border border-purple-200 flex-grow p-2"
-                type="text"/>
-                <Button>Add todo</Button>
-            </div>
-            {
-                res.data?.map((todo) => {
-                    return (
-                        <Link key={todo.id} href={`/todo/${todo.id}`}>
-                        <h1 className="text-center my-5 itms-center border text-3xl w-2/3 mx-auto">{todo.todo}</h1>
-                    </Link>
-                    )
-                })
-            }
-        </div>
-    );
-}
+//     let res = await fetch("http://localhost:3000/api/todos",{
+//         cache:"no-cache"
+//     })
+//     res = await res.json();
+//     return (
+//         <div className="min-h-screen p-10">
+//             <h1 className="text-3xl font-bold text-center">This is Todos page</h1>
+//             <div className="w-2/3 mx-auto flex gap-2 ">
+//                 <input
+//                 placeholder="Add todo"
+//                 className="border border-purple-200 flex-grow p-2"
+//                 type="text"/>
+//                 <Button>Add todo</Button>
+//             </div>
+//             {
+//                 res.data?.map((todo) => {
+//                     return (
+//                         <Link key={todo.id} href={`/todo/${todo.id}`}>
+//                         <h1 className="text-center my-5 itms-center border text-3xl w-2/3 mx-auto">{todo.todo}</h1>
+//                     </Link>
+//                     )
+//                 })
+//             }
+//         </div>
+//     );
+// }
